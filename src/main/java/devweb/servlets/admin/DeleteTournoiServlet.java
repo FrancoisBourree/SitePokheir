dc784c9c11875be3cd80a2ce7c62211cdda6484d
Bourree <francois-bourree@orange.fr>
2018-06-06 19:13:31 +0200

    Sélection, vérification, suppression de tournoi
    Vérification en JEE que l’on reçoit bien un tournoi créé depuis la liste déroulante.
    Suppression du tournoi

--- src/main/java/devweb/servlets/admin/DeleteTournoiServlet.java
+++ src/main/java/devweb/servlets/admin/DeleteTournoiServlet.java
@@ -1,10 +1,6 @@
 package devweb.servlets.admin;
 
-import devweb.entities.Article;
-import devweb.services.ArticleService;
-import devweb.servlets.GenericServlet;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
+import devweb.services.TournoiService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,11 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet("/deltournoi")
 
-    public class DeleteTournoiServlet extends HttpServlet { //crée une servlet
+    public class DeleteTournoiServlet extends HttpServlet { //créé une servlet
 
         @Override
         // Requête qui permet d'envoyer des infos
@@ -24,11 +19,11 @@ import java.util.List;
             Integer id = Integer.parseInt(req.getParameter("id-Tournoi"));
 
             try {
-                ArticleService.getInstance().delArticle(id);
+                TournoiService.getInstance().delTournoi(id);
             } catch (IllegalArgumentException e) {
                 req.getSession().setAttribute("delTournoiErrorMessage", e.getMessage());
             }
-            resp.sendRedirect("accueiladmin");
+            resp.sendRedirect("compteadmin");
         }
 
 
